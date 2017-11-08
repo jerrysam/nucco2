da2a9995dd6af86af5c448f7a7e000af2883c3ce
Jasper Lyons <jasper.lyons@gmail.com>
2017-11-08 09:54:45 +0000

    Make custom php task

--- config/task-config.js
+++ config/task-config.js
@@ -1,4 +1,14 @@
+const browserSync    = require('browser-sync')
+const data           = require('gulp-data')
+const gulp           = require('gulp')
+const handleErrors   = require('../node_modules/blendid/gulpfile.js/lib/handleErrors')
+const htmlmin        = require('gulp-htmlmin')
+const path           = require('path')
+const nunjucksRender = require('gulp-nunjucks-render')
+const fs = require('fs')
+
 module.exports = {
+  html        : true,
   images      : true,
   fonts       : true,
   static      : true,
@@ -6,16 +16,42 @@ module.exports = {
   ghPages     : true,
   stylesheets : true,
 
-  html : {
-    extensions: ['html', 'php'],
-    htmlmin: {
-      //ignoreCustomFragments: [/<\?php.*?\?>/]
+  additionalTasks: {
+    initialize(gulp, PATH_CONFIG, TASK_CONFIG) {
+      gulp.task('php', function () {
+        const exclude = '!' + path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.html.src, '**/{' + TASK_CONFIG.html.excludeFolders.join(',') + '}/**');
+
+        const paths = {
+          src: [path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.html.src, '**/*.php'), exclude],
+          dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest),
+        }
+
+        const dataFunction = TASK_CONFIG.html.dataFunction || function(file) {
+          const dataPath = path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.html.src, TASK_CONFIG.html.dataFile)
+          return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
+        }
+
+        const nunjucksRenderPath = [ path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.html.src) ]
+        TASK_CONFIG.html.nunjucksRender.path = TASK_CONFIG.html.nunjucksRender.path || nunjucksRenderPath
+
+        return gulp.src(paths.src)
+          .pipe(data(dataFunction))
+          .on('error', handleErrors)
+          .pipe(nunjucksRender(Object.assign({ inheritExtension: true }, TASK_CONFIG.html.nunjucksRender)))
+          .on('error', handleErrors)
+          .pipe(gulp.dest(paths.dest))
+          .pipe(browserSync.stream())
+      });
     },
-    nunchucksRender: {
-      inheritExtension: true
+    development: {
+      prebuild: ['php'],
+      postbuild: null
+    },
+    production: {
+      prebuild: ['php'],
+      postbuild: null
     }
   },
-
   javascripts: {
     entry: {
       // files paths are relative to
@@ -32,6 +68,6 @@ module.exports = {
   },
 
   production: {
-    rev: true
+    rev: false
   }
 }
